fc01c41b81b5a8c967c9282c2b8630dfae0e70b8
Yichen Niu <yn2467@nyu.edu>
2025-09-16 11:58:23 -0400

    WEEK2 notes when doing my HMWK. Helping me remember the mistakes I made.

--- week2/README.md
+++ week2/README.md
@@ -1,12 +1,5 @@
-# template
+# WEEK2 Notes
 
----
-
-This is a README file that can be used to describe and document your assignment.
-
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
-
----
 
 # Heading1
 ## Heading2
@@ -17,45 +10,8 @@ Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://w
 
 **bold text**
 
-*italicized text*
-
-~~strikethrough text~~
-
-Ordered List:
-1. First item
-2. Second item
-3. Third item
-
-Unordered List:
-- First item
-- Second item
-- Third item
-
-`short code block`
-
-```
-extended code block
-fun() {
-  return 0
-}
-```
-
-Link:  
-[linked text](https://www.example.com)
-
-
-Image with url:  
-![image description](https://dm-gy-6063-2024f-b.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
-
-
-Image on repo:  
-![image description](./file-name.jpg)
-
-
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
-
-
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
+## Stoke: Modify Stroke Properties
+## Shapes: circle/rectangle/triangle/elipse/line to create different shapes
+## (x,y,diamter/size): when writing number behind a shape, its always its location (x,y) and then how big the shape is.
+## Fill: Fill out the shape with a specific RGB color. Use this **BEFORE** the shape function.
+## No Fill/Stroke: Do not use any fill or stroke on shapes **CAPITALIZE** F and S when using noFill and noStroke.
--- week2/sketch.js
+++ week2/sketch.js
@@ -1,10 +1,41 @@
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600,600); // Create a canvas that fills the window
+  background(185,242,78); // Set the background color
   
 }
 
 function draw() {
+  strokeWeight(weight=3); // Set the stroke weight
+  fill(255,73,33); // Set the fill color to red
+  stroke(0,222,33); // Set the stroke color to green
+  circle(150,100,40); // Draw a circle
   
+  stroke(0,222,33); // Set the stroke color to green
+  noFill(); // Disable fill for the next shape
+  circle(50+150,100,40); // Draw a circle without fill
+
+  stroke(0,223,233); // Set the stroke color to green
+  noFill(); // Disable fill for the next shape
+  circle(50+150,100+100,150); // Draw a circle without fill
+
+  stroke(248,222,116); // Set the stroke color to yellow
+  strokeWeight(weight=10); // Set the stroke weight
+  fill(243,22,200); // Set the fill color to purple
+  triangle(300,300,400,300,350,200); // Draw a triangle
+  
+  noStroke(); // Set the stroke color to yellow
+  fill(23,0,200); // Set the fill color to purple
+  triangle(300-100,300+200,400+300,300-200,350,200+100); // Draw a triangle
+
+  strokeWeight(weight=1); // Set the stroke weight
+  fill(33,150,243); // Set the fill color to blue
+  stroke(248,30,22); // Set the stroke color to purple
+  rect(200,250,100,150); // Draw a rectangle
+  
+  strokeWeight(weight=1); // Set the stroke weight
+  noFill(); // Disable fill for the next shape
+  stroke(20,200,3); // Set the stroke color to purple
+  rect(200+30,250-90,150,70); // Draw a rectangle
 }
