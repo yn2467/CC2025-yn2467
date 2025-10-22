11362e58daaa364aacc07edf6a87a1103f624643
Yichen Niu <yn2467@nyu.edu>
2025-10-22 16:29:04 -0400

    adding week 6/7/8 notes

--- week6array/sketch.js
+++ week6array/sketch.js
@@ -1,9 +1,5 @@
-let x = 0;
-let y = 0;
-let d = 20;
-let speed = 5;
-let hue = 20;
-let opacity = 127;
+let drunks = [];
+let drunkAmount = 50;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,12 +8,55 @@ function setup() {
   y = height/2;
   colorMode(HSB);
 
-  
+  for(i=0; i<drunkAmount; i++){
+    let drunkD = random(10,100);
+    let drunkSpeed = random(1,7);
+    let drunkHue = random(0,60); 
+    drunks[i] = new Drunk(width/2,height/2,drunkD,drunkAmount,drunkHue);
+  }
 }
 
 function draw() {
-  x = x + random(-speed, speed);
-  y = y + random(-speed, speed);
-  fill(hue,70,100,opacity);
-  circle(x,y,d);
+  for(let i = 0; i<drunks.length; i++){
+    drunks[i].move();
+    drunks[i].drawDrunk();
+
+  }
+  // use customize function with ();
+  // Its also using corresponding variable defined in our customize
+  // function. Eg.: drunkSpeed = 9, drunkHue = 50, drunkDiameter =50)
+}
+
+// customized function
+function drawDrunk(drunkSpeed,drunkHue,drunkDiameter){
+  x = x + random(-drunkSpeed, drunkSpeed);
+  y = y + random(-drunkSpeed, drunkSpeed);
+  fill(drunkHue,70,100,opacity);
+  circle(x,y,drunkDiameter);
+}
+
+// Notes:
+// CLASS: factory for objects
+// OBJECT: assenblage of variables + functions
+
+class Drunk {
+  constructor(x,y,diameter,speed,hue){
+    this.x = x;
+    this.y = y;
+    this.diameter = diameter;
+    this.speed = speed;
+    this.hue = hue;
+    this.opacity = random(0,.4); //you can also initialize variables
+  }
+
+  move(){ // you can dclare functions or "methods" like this
+    this.x = this.x+random(-this.speed,this.speed);
+    this.y = this.y+random(-this.speed,this.speed);
+
+  }
+
+  drawDrunk(){
+    fill(this.hue,70,100,this.opacity);
+    circle(this.x,this.y,this.diameter);
+  }
 }
